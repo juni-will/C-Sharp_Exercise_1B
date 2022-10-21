020eebf3a8a1045abf1dc122575c419d60b1299a
Juniper Williams <54290862+juni-will@users.noreply.github.com>
2022-10-20 22:53:40 -0400

    C# Program
    This program asks for users to input a: square length, cube length, and circle radius. The program outputs the: area of a square and circle, volume of cube, perimeter of square and circumference of circle.

---
+++ Exercise1B/Exercise1B/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            double slen;
+            double clen;
+            double pi = 3.14;
+            Console.WriteLine("Alyssa Hanson");
+            Console.WriteLine("Welcome to C#\n");
+
+            Console.WriteLine("Enter Length of Square: \n");
+            slen = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The Area of The Square is: {slen * slen}\n");
+
+            Console.WriteLine("Enter Length of Cube: \n");
+            clen = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The Volume of The Cube is: {clen * clen * clen}\n");
+
+            Console.WriteLine("Enter Radius of Circle: \n");
+            radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The Area of The Circle: {pi * radius * radius}\n");
+
+            Console.WriteLine($"The Perimeter of The Square is: {4 * slen}\n");
+
+            Console.WriteLine($"The Circumference of The Circle is: {2 * pi * radius}\n");
+
+        }
+    }
+}
